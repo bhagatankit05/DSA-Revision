b8cac66e03a787f9ebae48f87217c6b4a70a1201
bhagatankit05 <bhagatankit82@gmail.com>
2025-08-18 20:43:14 +0530

    implemented selection sort

---
+++ Sorting_Techniques/Selection_Sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        // find smallest element in unsorted part.
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original Array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    selectionSort(arr, n);
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
