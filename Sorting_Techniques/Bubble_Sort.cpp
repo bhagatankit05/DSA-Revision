81b9ee59bd47e567d2aefb2c8ca4a9fa55ab97aa
bhagatankit05 <bhagatankit82@gmail.com>
2025-08-18 06:31:07 +0530

    implemented bubble sort

---
+++ Sorting_Techniques/Bubble_Sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main(){
+
+    int arr[]={45,98,78,65,32,45,78};
+    int n= sizeof(arr)/sizeof(arr[0]);
+
+    cout<<"Original Array : ";
+    for (int i = 0; i < n ; i++)
+        cout<<arr[i]<<" ";
+    cout<<endl;
+
+    bubbleSort(arr,n);
+
+    cout<<"Sorted Array : ";
+    for (int i = 0; i < n ; i++)
+        cout<<arr[i]<<" ";
+    cout<<endl;
+
+    return 0;
+}
